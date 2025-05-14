b76f36958b5fd7936d6798f9dbbbd794b82a7f7b
Higor Braga <higorbraga.s@gmail.com>
2025-05-13 23:34:13 -0300

    feat: hero and button component creation

---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+import { RiArrowRightUpLine } from "react-icons/ri";
+
+interface ButtonProps {
+   children: React.ReactNode;
+}
+
+const Button = ({ children }: ButtonProps) => {
+   return (
+      <button className="w-[120px] h-[54px] py-[5px] pl-[10px] pr-[5px] flex items-center justify-between min-w-[200px] group bg-accent">
+         <div className="flex-1 text-center tracking-[1.2px] font-primary font-bold text-primary text-sm uppercase">
+            {children}
+         </div>
+         <div className="w-11 h-11 bg-primary flex items-center justify-center">
+            <RiArrowRightUpLine className="text-white text-xl group-hover:rotate-45 transition-all duration-200" />
+         </div>
+      </button>
+   );
+};
+
+export default Button;
